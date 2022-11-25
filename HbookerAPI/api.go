9866f0488b852a1024cff6f7c85f61e8ab202fa3
VeronicaAlexia <Elaina-Alex@proton.me>
2022-11-25 18:59:52 +0800

    ~

--- HbookerAPI/api.go
+++ HbookerAPI/api.go
@@ -42,6 +42,26 @@ func GET_SEARCH(KeyWord string, page int) HbookerStruct.Search {
 
 }
 
+func GET_TAG_BOOK(page int) HbookerStruct.Search {
+	var search HbookerStruct.Search
+	params := map[string]string{
+		"filter_word":    "",
+		"count":          "10",
+		"tags":           `[{"filter":"1","tag":""}]`,
+		"use_daguan":     "0",
+		"page":           strconv.Itoa(page),
+		"is_paid":        "",
+		"category_index": "0",
+		"key":            "",
+		"filter_uptime":  "",
+		"up_status":      "",
+		"order":          "uptime",
+	}
+	NewHttpUtils(BOOKCITY_GET_FILTER_LIST, "POST").params(params).NewRequests().Unmarshal(&search)
+	return search
+
+}
+
 func GET_LOGIN_TOKEN(account, password string) *HbookerStruct.Login {
 	// hbooker new version add GEETEST verification, if you enter the wrong information or log in multiple times, GEETEST verification will be triggered.
 	// IP address may need to log in again after a few hours to avoid triggering verification, you can try to change the IP to avoid triggering verification.
